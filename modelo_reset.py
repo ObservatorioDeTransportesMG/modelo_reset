3f5c686adb8dcebfe156261170b509e1a68c1de4
yagomaia <yagohenriquev123@gmail.com>
2025-05-19 17:24:49 -0300

    first commit

---
+++ modelo_reset.py
@@ -0,0 +1,27 @@
+
+
+class ModeloReset:
+	def __init__(self):
+		pass
+
+	def _ler_shapefile(self, path: str, crs: int):
+		shapefile = 
+		pass
+
+	def _ler_dados_estatisticos(self, path: str):
+		pass
+
+	def mostrar_polos(self):
+		pass
+
+	def mostrar_zonas_atratoras(self):
+		pass
+
+	def mostrar_zonas_produtoras(self):
+		pass
+
+	def mostrar_densidade_renda(self):
+		pass
+
+	def mostrar_ponto_de_articulacao(self):
+		pass
